9d7ad0b6531137df87aab70cfad2310c1b6386a5
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-02-02 19:56:47 +0200

    feat: modify backend URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://itunes-search-backend-nt0u.onrender.com' || 'http://localhost:5000', // Base URL for API calls.
+    baseURL: 'https://itunes-search-backend-nt0u.onrender.com', // Base URL for API calls.
 });
 
 export default api;
