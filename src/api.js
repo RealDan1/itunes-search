3281816c3503a383f6843b4aaad7e8c3428e7424
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-04-16 09:58:08 +0200

    feat: add auto swithc url for render and local dev

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/', // Base URL for API calls.
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5001/'
+      : 'https://itunes-search-backend-nt0u.onrender.com', // Base URL for API calls.
 
-  //when switch to render, do this: "https://itunes-search-backend-nt0u.onrender.com"
+  //when switch to render, use: "https://itunes-search-backend-nt0u.onrender.com"
 });
 
 export default api;
