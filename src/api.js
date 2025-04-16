2033e2e654ae8a5a9e637f08f0fad5a18839007d
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-04-16 15:05:45 +0200

    feat: modify readme for switching baseURL

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://itunes-search-backend-nt0u.onrender.com', // Base URL for API calls.
+  baseURL: 'https://itunes-search-backend-nt0u.onrender.com', // Base URL for API calls. switch to 'http://localhost:5001/' if using local dev environment
 
   //when switch to render, do this: "https://itunes-search-backend-nt0u.onrender.com"
-  // use for auto switch:baseURL: process.env.NODE_ENV === 'development'
+  // use for auto switch:
+  // baseURL: process.env.NODE_ENV === 'development'
   //? 'http://localhost:5001/'
   //: 'https://itunes-search-backend-nt0u.onrender.com',
 });
