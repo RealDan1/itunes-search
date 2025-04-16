e523a91210e6b26e353c53351197c4924212fe03
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-04-16 12:00:10 +0200

    feat: switch back to render baseUrl

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:5001/'
-      : 'https://itunes-search-backend-nt0u.onrender.com', // Base URL for API calls.
+  baseURL: 'https://itunes-search-backend-nt0u.onrender.com', // Base URL for API calls.
 
-  //when switch to render, use: "https://itunes-search-backend-nt0u.onrender.com"
+  //when switch to render, do this: "https://itunes-search-backend-nt0u.onrender.com"
+  // use for auto switch:baseURL: process.env.NODE_ENV === 'development'
+  //? 'http://localhost:5001/'
+  //: 'https://itunes-search-backend-nt0u.onrender.com',
 });
 
 export default api;
